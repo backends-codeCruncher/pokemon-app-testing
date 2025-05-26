d8ed21e33ce0b188fd6a6c375776b43ca5af4962
Sergio Barreras <sergiobg.isc@gmail.com>
2025-05-26 06:14:35 -0700

    Test - Pagination.dto

---
+++ src/shared/dtos/pagination.dto.spec.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata';
+
+import { validate } from 'class-validator';
+import { PaginationDto } from './pagination.dto';
+
+describe('pagination.dto.ts', () => {
+  it('should validate with default values', async () => {
+    const dto = new PaginationDto();
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should validate with valid data', async () => {
+    const dto = new PaginationDto();
+    dto.limit = 10;
+    dto.page = 1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should not validate with invalid page', async () => {
+    const dto = new PaginationDto();
+    dto.page = -1;
+
+    const errors = await validate(dto);
+
+    errors.forEach((error) => {
+      if (error.property === 'page') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+
+  it('should not validate with invalid limit', async () => {
+    const dto = new PaginationDto();
+    dto.limit = -10;
+
+    const errors = await validate(dto);
+
+    errors.forEach((error) => {
+      if (error.property === 'limit') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+});
