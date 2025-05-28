df9dc1f67cf0a41372d19bc610a79f23c4a31725
Sergio Barreras <sergiobg.isc@gmail.com>
2025-05-27 19:55:37 -0700

    Test - Main.ts

---
+++ src/main.spec.ts
@@ -0,0 +1,71 @@
+import { NestFactory } from '@nestjs/core';
+import { bootstrap } from './main';
+import { AppModule } from './app.module';
+
+jest.mock('@nestjs/core', () => ({
+  NestFactory: {
+    create: jest.fn().mockResolvedValue({
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    }),
+  },
+}));
+
+describe('main.ts', () => {
+  let mockApp: {
+    useGlobalPipes: jest.Mock;
+    setGlobalPrefix: jest.Mock;
+    listen: jest.Mock;
+  };
+
+  beforeEach(() => {
+    mockApp = {
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    };
+
+    (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+  });
+
+  it('should create app', async () => {
+    await bootstrap();
+
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+
+  it('should set global prefix', async () => {
+    await bootstrap();
+
+    expect(mockApp.setGlobalPrefix).toHaveBeenCalledWith('api');
+  });
+
+  it('should listen on port 3000 if env port not set', async () => {
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(3000);
+  });
+
+  it('should listen to env port', async () => {
+    process.env.PORT = '4200';
+
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(process.env.PORT);
+  });
+
+  it('should use global pipes', async () => {
+    await bootstrap();
+
+    expect(mockApp.useGlobalPipes).toHaveBeenCalledWith(
+      expect.objectContaining({
+        errorHttpStatusCode: 400,
+        validatorOptions: expect.objectContaining({
+          forbidNonWhitelisted: true,
+          whitelist: true,
+        }),
+      }),
+    );
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -16,4 +16,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
